8cbb2d719f776a579153c79833d90cf82a136f4c
wevioz <wevioz@mail.ru>
2023-05-30 18:35:08 +0300

    Got the task done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,66 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        School school = new School();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        school.addStudent("Alex", "Garkaviy");
+        school.addTeacher("Sergei", "Streha");
+        school.addStudent("Eugene", "Soroka");
+        school.addTeacher("Vadim", "Lazuk");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        school.removeStudent("Alex", "Garkaviy");
+
+        school.students.forEach(student -> System.out.println(student.firstName));
+        school.teachers.forEach(teacher -> System.out.println(teacher.firstName));
+    }
+}
+
+class School {
+    ArrayList<Student> students = new ArrayList<Student>();
+    ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+
+    void addStudent(String firstName, String lastName) {
+        students.add(new Student(firstName, lastName));
+    }
+
+    void addTeacher(String firstName, String lastName) {
+        this.teachers.add(new Teacher(firstName, lastName));
+    }
+
+    void removeTeacher(String firstName, String lastName) {
+        this.teachers.removeIf(
+                teacher -> teacher.firstName.equals(firstName) &&
+                        teacher.lastName.equals(lastName)
+        );
+    }
+
+    void removeStudent(String firstName, String lastName) {
+        this.students.removeIf(
+                student -> student.firstName.equals(firstName) &&
+                        student.lastName.equals(lastName)
+        );
+    }
+}
+
+class Person {
+    String firstName;
+    String lastName;
+
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+class Student extends Person {
+    Student(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
+
+class Teacher extends Person {
+    Teacher(String firstName, String lastName) {
+        super(firstName, lastName);
     }
 }
