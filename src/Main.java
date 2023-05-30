4e2c23230fae54d578aef549e1a73288918b8425
wevioz <wevioz@mail.ru>
2023-05-30 10:36:49 +0300

    Got the task done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayOfNumbers = new ArrayList<Integer>();
+        LinkedList<Integer> arrayOfNumbers = new LinkedList<>();
 
         arrayOfNumbers.add(1);
         arrayOfNumbers.add(2);
         arrayOfNumbers.add(3);
 
-        Collections.reverse(arrayOfNumbers);
+        Collections.shuffle(arrayOfNumbers);
 
         arrayOfNumbers.forEach(System.out::println);
     }
