7ce742f1a9bfd36e03e7a88d882da92580d4e417
wevioz <wevioz@mail.ru>
2023-05-30 15:53:44 +0300

    Got the task done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        ArrayList<Integer> arrayOfNumbers = new ArrayList<Integer>();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Collections.addAll(arrayOfNumbers, 3, 4, 2, 6, 1, 9, 8);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        List<Integer> numbers = arrayOfNumbers.stream().sorted().toList();
+
+        System.out.printf("Second smallest: %d; Second biggest: %d", numbers.get(1),  numbers.get(numbers.size() - 2));
     }
 }
