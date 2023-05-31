199065c0b27ed80f6d243556dcd06030518e4d83
wevioz <wevioz@mail.ru>
2023-05-31 11:09:42 +0300

    BugFix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.stream.Collectors;
 
@@ -7,10 +8,10 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<String> arrayOfStrings = new ArrayList<String>();
 
-        Collections.addAll(arrayOfStrings, "Alpha", "Zoo", "Beta", "Awz");
+        Collections.addAll(arrayOfStrings, "Alpha", "Coma", "Zoo", "Beta", "Awz");
 
-        arrayOfStrings.stream().sorted().forEach(System.out::println);
+        arrayOfStrings.stream().sorted(Comparator.comparing(item -> new StringBuilder(item).reverse().toString())).forEach(System.out::println);
 
-        arrayOfStrings.stream().sorted(Collections.reverseOrder()).forEach(System.out::println);
+        arrayOfStrings.stream().sorted(Collections.reverseOrder(Comparator.comparing(item -> new StringBuilder(item).reverse().toString()))).forEach(System.out::println);
     }
 }
