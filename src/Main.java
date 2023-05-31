4e7139754d9e1e6550d79a7495432554c95055f8
wevioz <wevioz@mail.ru>
2023-05-31 10:12:33 +0300

    Got the task done

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,143 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        TravelAgency travelAgency = new TravelAgency();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        travelAgency.createFlight("BelAvia", 2023, Calendar.JULY, 27);
+        travelAgency.createFlight("BelAir", 2024, Calendar.JANUARY, 12);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        travelAgency.flights.forEach(System.out::println);
+
+        List<Flight> foundFlights = travelAgency.searchFlights("e");
+
+        foundFlights.forEach(flight -> System.out.printf("Flight #%d called %s goes %s \n", flight.id, flight.name, flight.flightDate.getTime()));
+
+        travelAgency.bookFlightById(0);
+        travelAgency.bookFlightById(1);
+
+        travelAgency.removeFlightById(0);
+
+        travelAgency.reservedFlights.forEach(flight -> System.out.println(flight.name));
+    }
+}
+
+interface BookingObject {
+    String getName();
+}
+
+class TravelAgency {
+    ArrayList<Flight> flights = new ArrayList<Flight>();
+    ArrayList<Flight> reservedFlights = new ArrayList<Flight>();
+    ArrayList<Hotel> hotels = new ArrayList<Hotel>();
+    ArrayList<Hotel> reservedHotels = new ArrayList<Hotel>();
+
+    <T> T findById(ArrayList<T> array, int id) {
+        return array.get(id);
+    }
+
+    <T extends BookingObject> List<T> search(ArrayList<T> array, String keyWord) {
+        return array.stream().
+                filter(item -> item.getName().toLowerCase().contains(keyWord.toLowerCase())).
+                toList();
+    }
+
+    void createFlight(
+            String flightName,
+            int flightYear,
+            int flightMonth,
+            int flightDay
+    ) {
+        Calendar date = new GregorianCalendar(flightYear, flightMonth, flightDay);
+
+        flights.add(new Flight(flightName, date));
+    }
+
+    List<Flight> searchFlights(String keyWord) {
+        return this.search(this.flights, keyWord);
+    }
+
+    Flight findFlightById(int id) {
+        return findById(this.flights, id);
+    }
+
+    void bookFlightById(int id) {
+        Flight targetFlight = this.findFlightById(id);
+
+        if (!this.reservedFlights.contains(targetFlight)) {
+            this.reservedFlights.add(targetFlight);
         }
+    }
+
+    void removeFlightById(int id) {
+        Flight targetFlight = this.findFlightById(id);
+
+        this.reservedFlights.removeIf(flights -> flights.name.equals(targetFlight.name));
+    }
+
+    List<Hotel> searchHotels(String keyWord) {
+        return this.search(this.hotels, keyWord);
+    }
+
+    Hotel findHotelById(int id) {
+        return this.findById(this.hotels, id);
+    }
+
+    void bookHotelById(int id) {
+        Hotel targetHotel = this.findById(this.hotels, id);
+
+        if (!this.reservedHotels.contains(targetHotel)) {
+            this.reservedHotels.add(targetHotel);
+        }
+    }
+
+    void removeHotelById(int id) {
+        Hotel targetHotel = this.findById(this.hotels, id);
+
+        this.reservedHotels.removeIf(hotel -> hotel.name.equals(targetHotel.name));
+    }
+}
+
+class Flight implements BookingObject {
+    String name;
+    int id;
+    static int nextId = 0;
+    Calendar flightDate;
+
+    public String getName() {
+        return this.name;
+    }
+
+    Flight(
+            String name,
+            Calendar flightDate
+    ) {
+        this.name = name;
+        this.id = Flight.nextId++;
+        this.flightDate = flightDate;
+    }
+}
+
+class Hotel implements BookingObject {
+    String name;
+    int id;
+    static int nextId = 0;
+    Calendar bookingDate;
+
+    public String getName() {
+        return name;
+    }
+
+    Hotel(
+            String name,
+            Calendar bookingDate
+    ) {
+        this.id = Hotel.nextId++;
+        this.name = name;
+        this.bookingDate = bookingDate;
     }
 }
